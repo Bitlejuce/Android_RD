93c555569a40d252856e9576a2a052d7e3fef65a
Bitlejuce <bitlejuce@gmail.com>
2018-09-15 11:03:20 +0300

    -

--- FirstStep/Servio/app/src/main/java/servio/rd/servio/MainActivity.java
+++ FirstStep/Servio/app/src/main/java/servio/rd/servio/MainActivity.java
@@ -9,40 +9,72 @@ import android.widget.TextView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import servio.rd.servio.pojo.Place;
+import servio.rd.servio.pojo.PlaceGroup;
+import servio.rd.servio.pojo.PlaceGroupSchema;
 import servio.rd.servio.pojo.PlaceUnion;
 import servio.rd.servio.pojo.PlaceUnions;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
-    private TextView textView;
+    private TextView placeUnions;
+    private TextView placeGroups;
+    private TextView placeGroupSchemas;
+    private TextView places;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
-        textView = findViewById(R.id.textView);
+        placeUnions = findViewById(R.id.place_unions);
+        placeGroups = findViewById(R.id.place_groups);
+        placeGroupSchemas = findViewById(R.id.place_groups_schemas);
+        places = findViewById(R.id.places);
     }
 
     public void getPlaces(View view) {
-        textView.setText("");
+        placeUnions.setText("");
         App.getApi().getResults().enqueue(new Callback<PlaceUnions>() {
             @Override
             public void onResponse(Call<PlaceUnions> call, Response<PlaceUnions> response) {
                 PlaceUnions placeUnions = response.body();
                 String placeUnionToShow = "";
-                for (PlaceUnion placeUnion : placeUnions.getPlaceUnions()){
+                String placeGroupsToShow = "";
+                String placeGroupSchemasToShow = "";
+                String placeToShow = "";
+                for (PlaceUnion placeUnion : placeUnions.getPlaceUnions()) {
                     placeUnionToShow += placeUnion.getName();
-                    placeUnionToShow+= "\n";
+                    placeUnionToShow += "\n";
+
+                    for (PlaceGroup placeGroup : placeUnion.getPlaceGroups()) {
+                        placeGroupsToShow += placeGroup.getName();
+                        placeGroupsToShow += "\n";
+
+                        for (PlaceGroupSchema placeGroupSchema : placeGroup.getPlaceGroupSchemas()) {
+                            placeGroupSchemasToShow += placeGroupSchema.getName();
+                            placeGroupSchemasToShow += "\n";
+
+                            for (Place place : placeGroupSchema.getPlaces()) {
+                                placeToShow += place.getName();
+                                placeToShow += "\n";
+                            }
+                        }
+                    }
+
+                    MainActivity.this.placeUnions.setText(placeUnionToShow);
+                    MainActivity.this.placeGroups.setText(placeGroupsToShow);
+                    MainActivity.this.placeGroupSchemas.setText(placeGroupSchemasToShow);
+                    MainActivity.this.places.setText(placeToShow);
                 }
-                textView.setText(placeUnionToShow);
             }
 
             @Override
             public void onFailure(Call<PlaceUnions> call, Throwable t) {
-                textView.setText(t.getMessage());
+                placeUnions.setText(t.getMessage());
             }
         });
     }
 }
+
